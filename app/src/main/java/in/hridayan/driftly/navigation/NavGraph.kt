8f19a26a193356d2564a67aa0b58820998fd59b2
dp-hridayan <hridayanofficial@gmail.com>
2025-05-01 13:01:26 +0530

    added transitions in screen navigation

--- app/src/main/java/in/hridayan/driftly/navigation/NavGraph.kt
+++ app/src/main/java/in/hridayan/driftly/navigation/NavGraph.kt
@@ -1,5 +1,13 @@
 package `in`.hridayan.driftly.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.navigation.compose.NavHost
@@ -8,6 +16,7 @@ import androidx.navigation.compose.rememberNavController
 import `in`.hridayan.driftly.calender.presentation.screens.CalendarScreen
 import `in`.hridayan.driftly.home.presentation.screens.HomeScreen
 import kotlinx.serialization.Serializable
+import kotlin.math.exp
 
 @Composable
 fun Navigation() {
@@ -17,11 +26,74 @@ fun Navigation() {
             navController = navController, startDestination = HomeScreen
         ) {
             composable<HomeScreen>(
+                exitTransition = {
+                    slideOutHorizontally(
+                        targetOffsetX = { -(it * 0.10f).toInt() },
+                        animationSpec = tween(
+                            durationMillis = 300,
+                            easing = FastOutSlowInEasing
+                        )
+                    ) + fadeOut(
+                        animationSpec = tween(
+                            durationMillis = 50,
+                            easing = LinearEasing
+                        )
+                    )
+                }
+                ,
+                popEnterTransition = {
+                    slideInHorizontally(
+                        initialOffsetX = { -(it * 0.15f).toInt() },
+                        animationSpec = tween(
+                            durationMillis = 500,
+                            easing = FastOutSlowInEasing
+                        )
+                    ) + fadeIn(
+                        initialAlpha = 0.5f,
+                        animationSpec = tween(
+                            delayMillis = 66,
+                            durationMillis = 50,
+                            easing = LinearEasing
+                        )
+                    )
+                }
             ) {
                 HomeScreen()
             }
 
-            composable<CalendarScreen> {
+            composable<CalendarScreen> (
+                enterTransition = {
+                    slideInHorizontally(
+                        initialOffsetX = { (it * 0.15f).toInt() },
+                        animationSpec = tween(
+                            durationMillis = 500,
+                            easing = FastOutSlowInEasing
+                        )
+                    ) + fadeIn(
+                        initialAlpha = 0.5f,
+                        animationSpec = tween(
+                            durationMillis = 350,
+                            delayMillis = 66,
+                            easing = LinearEasing
+                        )
+                    )
+                }
+                ,
+                popExitTransition = {
+                    slideOutHorizontally(
+                        targetOffsetX = { (it * 0.10f).toInt() },
+                        animationSpec = tween(
+                            durationMillis = 300,
+                            easing = FastOutSlowInEasing
+                        )
+                    ) + fadeOut(
+                        animationSpec = tween(
+                            durationMillis = 50,
+                            easing = LinearEasing
+                        )
+                    )
+                }
+            ){
                 CalendarScreen()
             }
         }
