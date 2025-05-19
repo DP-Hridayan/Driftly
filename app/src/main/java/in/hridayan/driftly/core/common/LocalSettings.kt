6a79cc545e535775e4d941356c84c2993cbf01be
dp-hridayan <hridayanofficial@gmail.com>
2025-05-19 15:19:36 +0530

    ignore

--- app/src/main/java/in/hridayan/driftly/core/common/LocalSettings.kt
+++ app/src/main/java/in/hridayan/driftly/core/common/LocalSettings.kt
@@ -2,6 +2,7 @@ package `in`.hridayan.driftly.core.common
 
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.runtime.compositionLocalOf
+import `in`.hridayan.driftly.core.common.constants.MATERIAL_BLUE
 import `in`.hridayan.driftly.settings.domain.model.SettingsState
 
 val LocalSettings = compositionLocalOf<SettingsState> {
@@ -9,6 +10,7 @@ val LocalSettings = compositionLocalOf<SettingsState> {
         isAutoUpdate = false,
         isDarkMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM,
         isHighContrastDarkMode = false,
+        seedColor = MATERIAL_BLUE,
         isDynamicColor = true,
         isHapticEnabled = true
     )
