52065413b18367cf5d8eec72f1650abc85f08b64
dp-hridayan <hridayanofficial@gmail.com>
2025-05-21 21:17:07 +0530

    ignore

--- app/src/main/java/in/hridayan/driftly/settings/data/remote/dto/GithubReleaseDto.kt
+++ app/src/main/java/in/hridayan/driftly/settings/data/remote/dto/GithubReleaseDto.kt
@@ -1,6 +1,7 @@
 package `in`.hridayan.driftly.settings.data.remote.dto
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class GitHubReleaseDto(
